3b78c0d00d8799d1bcc7f75ecc8bf0b73264e573
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-02-14 17:02:44 +0300

    cunt sentence

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+      self.value = value
+    
+    @property
+    def value(self):
+      return self._value
+  
+    @value.setter
+    def value(self, value):
+      if  isinstance(value, str):
+        self._value = value
+      else:
+        print("The value must be a string.")
+        self._value = ''
+    
+    def is_sentence(self):
+      return self._value.endswith('.')
+  
+    def is_question(self):
+      return self._value.endswith('?')
+  
+    def is_exclamation(self):
+      return self._value.endswith('!')
+  
+    def count_sentences(self):
+      temp_value = self._value.replace('!',".").replace('?',".")
+      sentences = [s.strip() for s in temp_value.split('.') if s.strip()]
+      return len(sentences)
+    
+
+string = MyString()
+string.value = "This is a string! It has three sentences. Right?"
+string.count_sentences()
+  
+
